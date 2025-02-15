b2c9170a6bfa8547846bb3afc98f9dee16ae35fe
rollinggir <joannetsengannu@gmail.com>
2025-02-15 22:36:06 +0530

    made it responsive

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="w-1/2 max-lg:w-full flex flex-col justify-center items-center">
 <h1 className='text-6xl max-lg:text-4xl font-light  text-center  tracking-tight leading-none'> Your Trusted Partner in Nursing Education & Career Growth</h1>
 <p className='text-center text-zinc-800 font-light mt-3 max-w-96 '>Join our nursing programs from reputed nursing colleges and gain the knowledge and skills needed to excel in the healthcare industry</p>
-<div className="w-full flex gap-4 justify-center items-center mt-6 ">
+<div className="w-full flex gap-4 justify-center items-center mt-6 max-md:flex-col ">
     <button className='border px-10 bg-black text-white  py-1 '><a href="tel:">Call me</a></button>
     <button className='px-10 py-1 border'><a href="mailto:">Email us</a></button>
 </div>
--- frontend/src/components/services/Footer.jsx
+++ frontend/src/components/services/Footer.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center bg-black text-white py-12 mt-32'>
+    <div className='flex justify-center items-center bg-black text-white py-12 mt-32 max-md:flex-col-reverse px-5'>
        
 
-<div className="flex gap-2 flex-col w-1/2 justify-center m-10 items-start">
+<div className="flex gap-2 flex-col w-1/2 justify-center m-10 items-start max-md:w-full max-md:text-center">
  <h4 className='text-2xl font-light'>Connect</h4>
  <p>Email: <a href="mailto:">www.example.com</a></p>
  <p>Phone: <a href="tel:">1234567890</a></p>
 </div>
-<div className="w-1/2  flex flex-col gap-5 m-3">
+<div className="w-1/2 max-md:w-full flex flex-col gap-5 m-3">
 <h3 className='text-5xl'>Talk to us</h3>
 <p className='font-light'>We provide expert guidance and support to aspiring nurses in India, helping them navigate their education and career journey with ease. From application assistance and career counseling to securing financial aid, we ensure you have the resources needed for a successful future in healthcare.</p>
-<button className='border-1 px-5 py-1 w-96'>Let's talk</button>
+<button className='border-1 px-5 py-1 w-96 max-md:w-64'>Let's talk</button>
 </div>
     </div>
   )
